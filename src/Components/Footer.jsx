ed77cbb2279047d2b0331a9eab60d8644392b843
Abdelrahman Samir -Semary <lesemary123@gmail.com>
2022-01-27 00:47:02 +0200

    Finishing Todo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import {Toolbar, Typography, Box, AppBar, List, ListItem,TextField, Button, IconButton, Grid} from "@mui/material";
+import variables from "../assets/App.scss";
 
 function Footer() {
-  return <div>
-      Footer
-  </div>;
+    return (
+        <Box>
+            <AppBar position="static" sx={{ backgroundColor:variables.mainColor }}>
+                <Grid container spacing={2} sx={{p: 2}}>
+                    <Grid item xs={6}>
+                        <List>
+                            <ListItem><Typography>Privacy Policy</Typography></ListItem>
+                            <ListItem><Typography>Terms</Typography></ListItem>
+                        </List>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <List>
+                            <ListItem><Typography>About Todo</Typography></ListItem>
+                            <ListItem><Typography>Wish it's good!</Typography></ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+            </AppBar>
+        </Box>
+
+    )
 }
 
 export default Footer;
--- src/Components/Views/Welcome.jsx
+++ src/Components/Views/Welcome.jsx
@@ -15,6 +15,8 @@ import {
   LinearProgress,
   Divider,
 } from "@mui/material";
+import CheckSVG from "../../assets/svg/checked.svg";
+import AllSVG from "../../assets/svg/all.svg";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { FcTodoList } from "react-icons/fc";
 import { styled } from "@mui/material/styles";
@@ -45,37 +47,33 @@ const ToggleTodoSwitch = styled(Switch)(({ theme }) => ({
       color: "#fff",
       transform: "translateX(22px)",
       "& .MuiSwitch-thumb:before": {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          "#fff"
-        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        backgroundImage: `url(${CheckSVG})`,
       },
       "& + .MuiSwitch-track": {
         opacity: 1,
-        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+        backgroundColor: "#aab4be",
       },
     },
   },
   "& .MuiSwitch-thumb": {
-    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    backgroundColor: variables.mainColor,
     width: 32,
     height: 32,
     "&:before": {
       content: "''",
       position: "absolute",
-      width: "100%",
-      height: "100%",
-      left: 0,
-      top: 0,
+      width: "60%",
+      height: "65%",
+      left: "7px",
+      top: "5px",
       backgroundRepeat: "no-repeat",
       backgroundPosition: "center",
-      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        "#fff"
-      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+      backgroundImage: `url(${AllSVG})`,
     },
   },
   "& .MuiSwitch-track": {
     opacity: 1,
-    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    backgroundColor: "#aab4be",
     borderRadius: 20 / 2,
   },
 }));
@@ -120,22 +118,18 @@ function Welcome() {
     // Set the user id to User State
     setUser({ ...User, [e.target.name]: e.target.value });
 
-    // Filter todos from user id provided
-    setTodos(Todos.filter((item) => item.userId === e.target.value));
+    if (e.target.value !== "") {
+      const filteredData = Todos.filter((item) => {
+        return item.userId === e.target.value;
+      });
+      setFilteredTodos(filteredData);
+    } else {
+      setFilteredTodos([]);
+    }
   };
   // Toggling Completed Todo
   const handleCompletedChange = (e) => {
     // Toggle todos completed/All
-
-    // const dataToShow = filter
-    // ? data.filter(d => d.id === filter)
-    // : data
-
-    // TodosType.checked ?
-    // setTodos((prevTodos) => ({...prevTodos})) :
-    // setTodos(Todos.filter((item) => item.completed !== TodosType.checked))
-
-    // return Todos.filter((item) => item.completed !== TodosType.checked);
 
     if (!TodosType.checked) {
       const filteredData = Todos.filter((item) => {
@@ -143,7 +137,7 @@ function Welcome() {
       });
       setFilteredTodos(filteredData);
     } else {
-      setFilteredTodos(Todos);
+      setFilteredTodos([]);
     }
 
     // Change checked button state
@@ -250,10 +244,11 @@ function Welcome() {
     setTodo({ ...Todo, [e.target.name]: e.target.value });
   };
   const TodoSubmitHandler = (e) => {
+    e.preventDefault();
     setTodo({ ...Todo, error_list: null });
     const data = {
-      title: Todo.title,
       userId: Todo.userId,
+      title: Todo.title,
       completed: false,
     };
     axios
@@ -264,37 +259,7 @@ function Welcome() {
           message: "You created a new todo!",
         });
 
-        let row = TodoTable.current.insertRow(-1);
-        let Td1 = row.insertCell(0);
-        let Td2 = row.insertCell(1);
-        let Td3 = row.insertCell(2);
-        let Td4 = row.insertCell(3);
-        let Td5 = row.insertCell(4);
-        Td1.innerHTML = response.data.id;
-        Td2.innerHTML = Todo.userId;
-        Td3.innerHTML = Todo.title;
-        Td4.innerHTML = ReactDOMServer.renderToString(
-          <VscError size={30} color={variables.errorColor} />
-        );
-        Td5.innerHTML = ReactDOMServer.renderToString(
-          <>
-            <IconButton
-              aria-label="delete"
-              color="error"
-              onClick={() => DeleteTodo(response.data.id)}
-            >
-              <RiDeleteBin6Fill />
-            </IconButton>
-            <IconButton
-              aria-label="complete"
-              color="success"
-              onClick={() => CompleteTodo(Todo.data.id)}
-            >
-              <BsCheckLg size={17} />{" "}
-            </IconButton>
-          </>
-        );
-
+        setTodos([...Todos, Todo]);
         setTodo({ ...initialTodo });
       })
       .catch(function (error) {
@@ -319,54 +284,6 @@ function Welcome() {
         });
       });
   }, []);
-
-  // let TodosSection = null;
-  // if (Object.keys(Todos).length > 0) {
-  //   TodosSection = currentTodos.map((Todo, index) => {
-  //     return (
-  //       <tr key={Todo.id} ref={(el) => (CurrentTodo.current[Todo.id] = el)}>
-  //         <td>{Todo.id}</td>
-  //         <td>{Todo.userId}</td>
-  //         <td>{Todo.title}</td>
-  //         {Todo.completed ? (
-  //           <td data-type="completed">
-  //             <FaClipboardCheck size={30} color={variables.successColor} />
-  //           </td>
-  //         ) : (
-  //           <td data-type="uncompleted">
-  //             <VscError size={30} color={variables.errorColor} />
-  //           </td>
-  //         )}
-  //         <td>
-  //           <IconButton
-  //             aria-label="delete"
-  //             color="error"
-  //             onClick={() => DeleteTodo(Todo.id)}
-  //           >
-  //             <RiDeleteBin6Fill />
-  //           </IconButton>
-  //           {Todo.completed ? (
-  //             <IconButton
-  //               aria-label="complete"
-  //               color="error"
-  //               onClick={() => CompleteTodo(Todo.id)}
-  //             >
-  //               <MdClose size={17} />
-  //             </IconButton>
-  //           ) : (
-  //             <IconButton
-  //               aria-label="complete"
-  //               color="success"
-  //               onClick={() => CompleteTodo(Todo.id)}
-  //             >
-  //               <BsCheckLg size={17} />{" "}
-  //             </IconButton>
-  //           )}
-  //         </td>
-  //       </tr>
-  //     );
-  //   });
-  // }
 
   // Getting users list
   useEffect(() => {
@@ -479,7 +396,7 @@ function Welcome() {
                 </th>
                 <th>
                   <HiOutlineUser />
-                  User ID
+                  User
                 </th>
                 <th>
                   <RiTodoLine /> Title
@@ -502,7 +419,13 @@ function Welcome() {
                       ref={(el) => (CurrentTodo.current[Todo.id] = el)}
                     >
                       <td>{Todo.id}</td>
-                      <td>{Todo.userId}</td>
+                      <td>
+                        {Object.keys(Users).length > 1
+                          ? Users.find((o) => o.id === Todo.userId).name +
+                            " : " +
+                            Todo.userId
+                          : Todo.userId}
+                      </td>
                       <td>{Todo.title}</td>
                       {Todo.completed ? (
                         <td data-type="completed">
@@ -553,7 +476,13 @@ function Welcome() {
                       ref={(el) => (CurrentTodo.current[Todo.id] = el)}
                     >
                       <td>{Todo.id}</td>
-                      <td>{Todo.userId}</td>
+                      <td>
+                        {Object.keys(Users).length > 1
+                          ? Users.find((o) => o.id === Todo.userId).name +
+                            " : " +
+                            Todo.userId
+                          : Todo.userId}
+                      </td>
                       <td>{Todo.title}</td>
                       {Todo.completed ? (
                         <td data-type="completed">
@@ -644,47 +573,49 @@ function Welcome() {
           <Typography variant="h6" color="initial">
             <RiTodoLine /> Create Your Todo
           </Typography>
-          <FormGroup>
-            <FormControl sx={{ marginBottom: "7px" }}>
-              <InputLabel id="userLabel">User</InputLabel>
-              <Select
-                label="User"
-                id="userLabel"
-                defaultValue={Todo.userId}
-                value={Todo.userId}
-                required
-                name="userId"
-                onChange={HandleCreateInput}
-              >
-                {UsersSection}
-              </Select>
-            </FormControl>
-            <FormControl sx={{ marginBottom: "7px" }}>
-              <TextField
-                placeholder="Write something you want to achieve..."
-                onChange={HandleCreateInput}
-                defaultValue={Todo.title}
-                value={Todo.title}
-                name="title"
-                required
-                rows={5}
-              />
-            </FormControl>
-            <FormControl sx={{ marginBottom: "7px" }}>
-              <Button
-                variant="outlined"
-                onClick={TodoSubmitHandler}
-                color="error"
-              >
-                {Todo.error_list === null ? (
-                  <CircularProgress size={20} />
-                ) : (
-                  <MdSave size={20} />
-                )}
-                &nbsp;Create New Todo
-              </Button>
-            </FormControl>
-          </FormGroup>
+          <Box component="form" autoComplete="true" onSubmit={TodoSubmitHandler}>
+            <FormGroup>
+              <FormControl sx={{ marginBottom: "7px" }}>
+                <InputLabel id="userLabel">User</InputLabel>
+                <Select
+                  label="User"
+                  id="userLabel"
+                  defaultValue={Todo.userId}
+                  value={Todo.userId}
+                  required
+                  name="userId"
+                  onChange={HandleCreateInput}
+                >
+                  {UsersSection}
+                </Select>
+              </FormControl>
+              <FormControl sx={{ marginBottom: "7px" }}>
+                <TextField
+                  placeholder="Write something you want to achieve..."
+                  onChange={HandleCreateInput}
+                  defaultValue={Todo.title}
+                  value={Todo.title}
+                  name="title"
+                  required={true}
+                  rows={5}
+                />
+              </FormControl>
+              <FormControl sx={{ marginBottom: "7px" }}>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  type="submit"
+                >
+                  {Todo.error_list === null ? (
+                    <CircularProgress size={20} />
+                  ) : (
+                    <MdSave size={20} />
+                  )}
+                  &nbsp;Create New Todo
+                </Button>
+              </FormControl>
+            </FormGroup>
+          </Box>
         </Grid>
       </Grid>
     </Box>
